c8aa6f109753c7f7e2d2d37a16e90f86e98a91c0
Greeblood <bloodgeme.mbm@hotmail.dk>
2026-02-24 22:29:35 +0100

    updatable navmenu

--- Services/UserService/UserSession.cs
+++ Services/UserService/UserSession.cs
@@ -8,8 +8,10 @@ namespace Store_Manager.Services.UserService
         public UserVm? CurrentUser { get; private set; }
         public bool IsLoggedIn => CurrentUser is not null;
         public bool IsAdmin() => CurrentUser?.Role == UserRole.Administrator;
+
+
         // Any component can subscribe to this event to be notified when the session changes.
-        // For example, NavMenu subscribes so it can re-render when the user logs in or out.
+        // currently im just using it for NavMenu when logging in.
         public event Action? OnChange;
 
         public void SetUser(UserVm user)
